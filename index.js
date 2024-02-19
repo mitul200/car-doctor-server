aa0d5e43436bb861bf46e04ce1004fa13613d013
mitul200 <mitul5267@gmail.com>
2024-02-19 13:36:29 +0600

    added CRAD opration find method

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const cors = require('cors');
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
-console.log(process.env.DB_PASS)
+console.log(process.env.DB_PASS);
 
 // middleWare ----//
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // docUser
 // coRZdkmRWtYMBIt1
@@ -22,29 +22,41 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db("carDoctor").collection("services");
+
+    app.get('/services', async(req,res)=>{
+        const cursor =serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send(
+    "doctors are busy in treatment that why we want to find new doctors"
+  );
+});
 
-
-app.get('/', (req ,res)=>{
-    res.send('doctors are busy in treatment that why we want to find new doctors')
-})
-
-app.listen(port,() =>{
-    console.log(`car doctor server is running ${port}`)
-})
+app.listen(port, () => {
+  console.log(`car doctor server is running ${port}`);
+});
