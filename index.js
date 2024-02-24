7f14c72723b0c1b7c1fcdb0394a7a545ab87523f
mitul200 <mitul5267@gmail.com>
2024-02-24 14:39:36 +0600

    explore crad oparation delet method

--- index.js
+++ index.js
@@ -43,18 +43,46 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const options ={
-        projection:{title: 1,price: 1,service_id: 1}
+        projection:{title: 1,price: 1,service_id: 1,img:1}
       }
       const result = await serviceCollection.findOne(query,options)
       res.send(result)
     })
 
     // bookings 
+
+
+    //  some data read//
+    app.get('/bookings',async (req,res)=>{
+      console.log(req.query);
+      let query = {}
+      if(req.query?.email){
+        query = {email:req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray()
+      res.send(result)
+
+    })
+
+
     app.post('/bookings', async (req,res) => {
       const booking = req.body
       const result = await bookingCollection.insertOne(booking)
       console.log(booking)
       res.send(result)
+    })
+
+    app.put('/bookings/:id', async(req,res)=>{
+      const updateedBookings = req.body
+
+    })
+
+    app.delete('/bookings/:id', async (req,res)=>{
+      const id= req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result)
+
     })
 
     // Send a ping to confirm a successful connection
